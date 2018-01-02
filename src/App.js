2dc9f676efc40af6d4c81afb0e582384867bf09f
Shwetank30 <shwetank.kumar.cer13@itbhu.ac.in>
2018-01-03 02:32:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import SlidingPane from 'react-sliding-pane';
+import 'react-sliding-pane/dist/react-sliding-pane.css'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPaneOpen: false,
+      isPaneOpenLeft: false
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <h1 className="App-title">Data-Analyser</h1>
+        </header>
+        <button className='slider' onClick={ () => this.setState({ isPaneOpen: true })}>Click to Open right pane!</button>
+        <SlidingPane
+          className='some-cus'
+          overlayClassName='kuch-aur'
+          isOpen={ this.state.isPaneOpen }
+          title='Hey, it is open'
+          subtitle='optional'
+          onRequestClose={ () => {
+            this.setState({ isPaneOpen: false });
+          }}
+        >
+          <div> Hum koi to hai</div>
+          <br />
+          <p> Koi hai </p>
+        </SlidingPane>
+        <p className="App-intro">
+          Important components incoming
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
